504f7380b1cbdf80dd9622fa2563674ab41be705
Diego Silva <diego.silva@nuveo.ai>
2020-12-06 09:21:07 -0300

    Fix DTO to interface with NPA

--- python-package/insightface/app/face_analysis.py
+++ python-package/insightface/app/face_analysis.py
@@ -86,7 +86,7 @@ class FaceAnalysis:
                         embedding=embedding,
                         gender=gender.item(),
                         age=age,
-                        normed_embedding=normed_embedding.item(),
-                        embedding_norm=embedding_norm)
+                        normed_embedding=normed_embedding,
+                        embedding_norm=embedding_norm.item())
             ret.append(face._asdict())
         return ret
